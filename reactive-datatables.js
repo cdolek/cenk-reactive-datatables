1331498c7ad0886cbeda1ffebf2c88b4476c6233
Cenk Dolek <cdolek@gmail.com>
2016-06-14 20:37:37 +0300

    initializing

---
+++ reactive-datatables.js
@@ -0,0 +1,5 @@
+// Write your package code here!
+
+// Variables exported by this module can be imported by other packages and
+// applications. See reactive-datatables-tests.js for an example of importing.
+export const name = 'reactive-datatables';
