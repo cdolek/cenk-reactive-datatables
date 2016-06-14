db30c4c012b6466c02ed4607f55cb29ba89593a8
Cenk Dolek <cdolek@gmail.com>
2016-06-14 20:44:48 +0300

    updated code to v1.0.0

--- package.js
+++ package.js
@@ -1,18 +1,21 @@
 Package.describe({
   name: 'cenk:reactive-datatables',
-  version: '0.0.1',
-  // Brief, one-line summary of the package.
-  summary: '',
-  // URL to the Git repository containing the source code for this package.
-  git: '',
-  // By default, Meteor will default to using README.md for documentation.
-  // To avoid submitting documentation, set this field to null.
+  version: '1.0.0',
+  summary: 'Reactive DataTables package for meteor with single row editing capability',
+  git: 'git@github.com:cdolek/cenk-reactive-datatables.git',
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.3.2.4');
-  api.use('ecmascript');
+  api.use(['templating'], 'client');
+  api.addFiles([
+  	'css/dataTables.1.10.11.min.css',
+    'jquery.dataTables.1.10.11.min.js',
+  	'reactive-datatables.js',
+  	'reactive-datatable-template.html',
+  	'reactive-datatable-template.js',
+  ], 'client');
   api.mainModule('reactive-datatables.js');
 });
 
--- reactive-datatables.js
+++ reactive-datatables.js
@@ -1,5 +1,83 @@
-// Write your package code here!
+ReactiveDatatable = function(options) {
+	var self = this;
 
-// Variables exported by this module can be imported by other packages and
-// applications. See reactive-datatables-tests.js for an example of importing.
-export const name = 'reactive-datatables';
+	this.options = options = _.defaults(options, {
+		// Any of these can be overriden by passing an options
+		// object into your ReactiveDatatable template (see readme)
+		rowId: '_id',
+		stateSave: true,
+		stateDuration: -1, // Store data for session only
+		pageLength: 10,
+		lengthMenu: [3, 5, 10, 50, 100],
+		columnDefs: [{ // Global default blank value to avoid popup on missing data
+			targets: '_all',
+			defaultContent: '–––'
+		}],
+		stateLoadParams: function(settings, data) {
+			// Make it easy to change to the stored page on .update()
+			self.page = data.start / data.length;
+		},
+
+		/*
+
+		"drawCallback": function( settings ) {
+        	var api = new $.fn.dataTable.Api( settings );
+
+	        // Output the data for the visible rows to the browser's console
+	        // You might do something more useful with it!
+	        // console.log( api.rows( {page:'current'} ).data() );
+
+	        console.log( settings );
+	    }
+
+	    */
+
+
+	});
+
+	// console.log("options?", options);
+};
+
+ReactiveDatatable.prototype.update = function(data, settings) {
+	if (!data) return;
+	var self = this;
+
+	/*
+	console.log("-----------------------------");
+	console.log("DataTables Data (Existing)");
+	console.log("-----------------------------");
+	console.log(self.datatable.rows().data());
+	console.log("-----------------------------");
+	console.log("dataTables updating");
+	console.log("-----------------------------");
+	console.log(data);
+	console.log("-----------------------------");
+	*/
+
+	var existingData = self.datatable.rows().data().toArray();
+	var dataDifference = self.difference(data,existingData);
+
+	if ( dataDifference.length > 0 ) {
+
+		dataDifference.forEach(function (dataItem) {
+
+			var index = self.datatable.row('#'+dataItem._id);
+
+			if ( index.length > 0 ) {
+				self.datatable.row(index[0]).data(dataItem).invalidate();
+			} else {
+				self.datatable.row.add(dataItem);
+			}
+
+		});
+
+	} else {
+
+		// Data initializing or we have the same data
+		self.datatable.rows.add(data);
+
+	}
+
+	self.datatable.draw(false);
+
+};
