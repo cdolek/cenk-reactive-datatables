d319308a197cbffd158339dc39de7161e0e8e5b8
Cenk Dolek <cdolek@gmail.com>
2017-03-04 16:13:09 -0500

    dataTables upgraded to v1.10.13

--- package.js
+++ package.js
@@ -1,27 +1,27 @@
 Package.describe({
-  name: 'cenk:reactive-datatables',
-  version: '1.0.0',
-  summary: 'Reactive DataTables package for meteor with single row editing capability',
-  git: 'git@github.com:cdolek/cenk-reactive-datatables.git',
-  documentation: 'README.md'
+    name: 'cenk:reactive-datatables',
+    version: '1.0.1',
+    summary: 'Reactive DataTables package for meteor with single row update capability',
+    git: 'git@github.com:cdolek/cenk-reactive-datatables.git',
+    documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.3.2.4');
-  api.use(['templating'], 'client');
-  api.addFiles([
-  	'css/dataTables.1.10.11.min.css',
-    'jquery.dataTables.1.10.11.min.js',
-  	'reactive-datatables.js',
-  	'reactive-datatable-template.html',
-  	'reactive-datatable-template.js',
-  ], 'client');
-  api.mainModule('reactive-datatables.js');
+    api.versionsFrom('1.3.2.4');
+    api.use(['templating'], 'client');
+    api.addFiles([
+        'css/dataTables.1.10.13.min.css',
+        'jquery.dataTables.1.10.13.min.js',
+        'reactive-datatables.js',
+        'reactive-datatable-template.html',
+        'reactive-datatable-template.js',
+    ], 'client');
+    api.mainModule('reactive-datatables.js');
 });
 
 Package.onTest(function(api) {
-  api.use('ecmascript');
-  api.use('tinytest');
-  api.use('cenk:reactive-datatables');
-  api.mainModule('reactive-datatables-tests.js');
+    api.use('ecmascript');
+    api.use('tinytest');
+    api.use('cenk:reactive-datatables');
+    api.mainModule('reactive-datatables-tests.js');
 });
