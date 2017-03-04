0cbc6b4cc1c5e91988136be3b56ba412253ff357
Cenk Dolek <cdolek@gmail.com>
2017-03-04 15:37:21 -0500

    fixed data removal bug

--- reactive-datatable-template.js
+++ reactive-datatable-template.js
@@ -23,31 +23,63 @@ Template.reactiveDatatable.rendered = function() {
         reactiveDataTable.page = info.page;
     });
 
-    dt.on('stateLoaded.dt', function (e, settings, data) {
+    dt.on('stateLoaded.dt', function(e, settings, data) {
         console.log('state loaded', settings, data);
-    } );
+    });
 
-    dt.on( 'stateLoadParams.dt', function (e, settings, data) {
+    dt.on('stateLoadParams.dt', function(e, settings, data) {
         console.log('stateLoadParams event fired', settings, data);
-    } );
+    });
 
 
     reactiveDataTable.datatable = dt;
 
 
-    reactiveDataTable.difference = function(array){
-       var rest = Array.prototype.concat.apply(Array.prototype, Array.prototype.slice.call(arguments, 1));
+    // reactiveDataTable.difference = function(array) {
+    //     var rest = Array.prototype.concat.apply(Array.prototype, Array.prototype.slice.call(arguments, 1));
 
-       var containsEquals = function(obj, target) {
-        if (obj == null) return false;
-        return _.any(obj, function(value) {
-          return _.isEqual(value, target);
+    //     var containsEquals = function(obj, target) {
+    //         if (obj == null) return false;
+    //         return _.some(obj, function(value) {
+    //             return _.isEqual(value, target);
+    //         });
+    //     };
+
+    //     var arr1 = _.pluck(array, "_id");
+    //     var arr2 = _.pluck(rest, "_id");
+    //     var diff = _.difference(arr1, arr2);
+
+    //     console.log('diff', diff);
+
+
+    //     return {
+    //         incremental: _.filter(array, function(value) { return !containsEquals(rest, value); }),
+    //         decremental: diff
+    //     };
+    // };
+
+    reactiveDataTable.difference = function(existingData, newData) {
+
+        var targetData = [],
+            existingDataIdArr = _.pluck(existingData, "_id"),
+            newDataIdArr = _.pluck(newData, "_id"),
+            diffIdArr = [];
+
+        if (existingData.length > newData.length) {
+            // remove
+            diffIdArr = _.difference(existingDataIdArr, newDataIdArr);
+            targetData = existingData;
+        } else {
+            // add
+            diffIdArr = _.difference(newDataIdArr, existingDataIdArr);
+            targetData = newData;
+        }
+
+        return _.filter(targetData, function(item) {
+            return _.contains(diffIdArr, item._id);
         });
-      };
 
-      return _.filter(array, function(value){ return ! containsEquals(rest, value); });
-    };
-
+    }
 
     this.autorun(function() {
         reactiveDataTable.update(data.tableData(), data.options);
--- reactive-datatables.js
+++ reactive-datatables.js
@@ -1,24 +1,24 @@
 ReactiveDatatable = function(options) {
-	var self = this;
+    var self = this;
 
-	this.options = options = _.defaults(options, {
-		// Any of these can be overriden by passing an options
-		// object into your ReactiveDatatable template (see readme)
-		rowId: '_id',
-		stateSave: true,
-		stateDuration: -1, // Store data for session only
-		pageLength: 10,
-		lengthMenu: [3, 5, 10, 50, 100],
-		columnDefs: [{ // Global default blank value to avoid popup on missing data
-			targets: '_all',
-			defaultContent: '–––'
-		}],
-		stateLoadParams: function(settings, data) {
-			// Make it easy to change to the stored page on .update()
-			self.page = data.start / data.length;
-		},
+    this.options = options = _.defaults(options, {
+        // Any of these can be overriden by passing an options
+        // object into your ReactiveDatatable template (see readme)
+        rowId: '_id',
+        stateSave: true,
+        stateDuration: -1, // Store data for session only
+        pageLength: 10,
+        lengthMenu: [3, 5, 10, 50, 100],
+        columnDefs: [{ // Global default blank value to avoid popup on missing data
+            targets: '_all',
+            defaultContent: '–––'
+        }],
+        stateLoadParams: function(settings, data) {
+            // Make it easy to change to the stored page on .update()
+            self.page = data.start / data.length;
+        },
 
-		/*
+        /*
 
 		"drawCallback": function( settings ) {
         	var api = new $.fn.dataTable.Api( settings );
@@ -33,51 +33,51 @@ ReactiveDatatable = function(options) {
 	    */
 
 
-	});
+    });
 
-	// console.log("options?", options);
+    // console.log("options?", options);
 };
 
 ReactiveDatatable.prototype.update = function(data, settings) {
-	if (!data) return;
-	var self = this;
+    if (!data) return;
+    var self = this;
 
-	/*
-	console.log("-----------------------------");
-	console.log("DataTables Data (Existing)");
-	console.log("-----------------------------");
-	console.log(self.datatable.rows().data());
-	console.log("-----------------------------");
-	console.log("dataTables updating");
-	console.log("-----------------------------");
-	console.log(data);
-	console.log("-----------------------------");
-	*/
+    var existingData = self.datatable.rows().data().toArray();
+    var dataDifference = self.difference(existingData, data);
 
-	var existingData = self.datatable.rows().data().toArray();
-	var dataDifference = self.difference(data,existingData);
+    if (data.length > existingData.length) {
+        /**
+         * ADD ROW
+         */
+        // console.log("//////////// ADD //////////////");
+        dataDifference.forEach(function(dataItem) {
 
-	if ( dataDifference.length > 0 ) {
+            var index = self.datatable.row('#' + dataItem._id);
 
-		dataDifference.forEach(function (dataItem) {
+            if (index.length > 0) {
+                self.datatable.row(index[0]).data(dataItem).invalidate();
+            } else {
+                self.datatable.row.add(dataItem);
+            }
 
-			var index = self.datatable.row('#'+dataItem._id);
+        });
 
-			if ( index.length > 0 ) {
-				self.datatable.row(index[0]).data(dataItem).invalidate();
-			} else {
-				self.datatable.row.add(dataItem);
-			}
-
-		});
-
-	} else {
-
-		// Data initializing or we have the same data
-		self.datatable.rows.add(data);
-
-	}
-
-	self.datatable.draw(false);
-
+    } else if (data.length < existingData.length) {
+        /**
+         * REMOVE ROW
+         */
+        // console.log("//////////// REMOVE //////////////");
+        dataDifference.forEach(function(dataItem) {
+            var index = self.datatable.row('#' + dataItem._id);
+            self.datatable.row('#' + dataItem._id).remove();
+        });
+    } else if (!existingData.length) {
+        /**
+         * INITIALIZE
+         */
+        // console.log("//////////// INIT //////////////");
+        // Data initializing or we have the same data
+        self.datatable.rows.add(data);
+    }
+    self.datatable.draw(false);
 };
